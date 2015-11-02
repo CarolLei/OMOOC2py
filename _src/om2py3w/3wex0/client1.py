16bd2131c1e90d773111b285bcd2882b4a846a0f
junjie_li <hunsjie@gmail.com>
2015-11-02 20:58:42 +0800

    Create client1.py

---
+++ _src/om2py3w/3wex0/client1.py
@@ -0,0 +1,26 @@
+# _*_ coding: utf-8 _*_
+# client1
+
+from sys import argv 
+import socket # 导入socket
+import time  # 引入时间模块
+
+history = open('all.txt')
+print history.read()
+history.close()
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # 创建socket，UDP类型
+
+
+while True:  # 利用while循环实现重复调用脚本效果
+  content = raw_input("content: ") # 输入日记内容
+
+  if content == '?':
+  	print '打印当前，帮助输入？'
+  	print '退出笔记，请按CTRL+C'
+  	continue
+
+  else:
+  	s.sendto(content, ('127.0.0.1', 9999))
+
+s.close() 
